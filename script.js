c62237f23fcc16ba48a79c320736b6c820d518c6
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-05 10:50:59 -0500

    Update script.js
    From "/codelit-learning/proxy-server/bloc/master/server.js"

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 const express = require('express')
 const puppeteer = require('puppeteer')
+const replace = require('absolutify')
 
 const app = express()
 
-app.get('/',async (req, res) => {
-  const {url} = req.query
-  
-  if(!url){
-    return res.send('No url Provided')
-  }
-  else {
-   try{
-     const browser = await puppeteer.launch()
-     const page = await broswer.newPage()
-     await page.goto('https://${url}')
-     
-     let document = await page.evaluate(() => document.document.Element.outerHTML)
-     
-     return res.send()
-   } catch (err){
-      return res.send(err)     
-   }      
-  }
+app.get('/', async (req, res) => {
+    const {url} = req.query
+    
+    if (!url) {
+        return res.send('Not url provided')
+    } else {
+        // generate puppeteer screenshot 
+        try {
+            // If headless Chrome is not launching on Debian, use the following line instead
+            // const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+            const browser = await puppeteer.launch()
+            const page = await browser.newPage()
+            await page.goto(`https://${url}`)
+            
+            let document = await page.evaluate(() => document.documentElement.outerHTML)
+            document = replace(document, `/?url=${url.split('/')[0]}`)
+            
+            return res.send(document)
+        } catch(err) {
+            console.log(err)
+            
+            return res.send(err)
+        }
+    }
 })
+
 
 app.listen(process.env.PORT)
