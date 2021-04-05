012c91f14c14585be363300eac8601e727f7a3b1
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-05 09:56:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("testing");
+const express = require('express')
+const puppeteer = require('puppeteer')
+
+const app = express()
+
+app.get('/',async (req, res) => {
+  return res.send('Hi')
+})
+
+app.listen(process.env.PORT)
