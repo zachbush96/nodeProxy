6331231af1015792eae955d77553d0d9eec28e0d
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-05 10:31:31 -0500

    Update script.js
    done server?

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const puppeteer = require('puppeteer')
 const app = express()
 
 app.get('/',async (req, res) => {
-  return res.send('Hi')
+  const {url} = req.query
+  
+  if(!url){
+    return res.send('No url Provided')
+  }
+  else {
+   try{
+     const browser = await puppeteer.launch()
+     const page = await broswer.newPage()
+     await page.goto('https://${url}')
+     
+     let document = await page.evaluate(() => document.document.Element.outerHTML)
+     
+     return res.send()
+   } catch (err){
+      return res.send(err)     
+   }      
+  }
 })
 
 app.listen(process.env.PORT)
